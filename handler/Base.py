03f1f431fb944625efb21846c15e478e179b1edd
MaGuowei <imaguowei@gmail.com>
2014-03-05 14:28:46 +0800

    添加 豆瓣电影信息

--- handler/Base.py
+++ handler/Base.py
@@ -1,7 +1,12 @@
 import tornado.web
+from util.movie import Movie
 
 
 class BaseHandler(tornado.web.RequestHandler):
     @property
     def db(self):
         return self.application.db
+
+    @property
+    def get_douban_movie(self):
+        return Movie().get_movie
--- handler/post.py
+++ handler/post.py
@@ -5,4 +5,4 @@ from bson.objectid import ObjectId
 class PostHandler(BaseHandler):
     def get(self, pid):
         post = self.db.movie.find_one({'_id': ObjectId(pid)})
-        self.render('post/index.html', post=post)
+        self.render('post/index.html', post=post, douban_movie=self.get_douban_movie(post['title']))

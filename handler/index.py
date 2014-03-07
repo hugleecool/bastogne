e8ac271a3e6f47c50dd9e95172f703eb047f3e6f
MaGuowei <imaguowei@gmail.com>
2014-03-07 22:28:46 +0800

    添加查询

--- handler/index.py
+++ handler/index.py
@@ -8,6 +8,29 @@ class IndexHandler(BaseHandler):
         self.render('index/index.html', posts=posts, page=page)
 
 
+class MovieHandler(BaseHandler):
+    def get(self):
+        page = int(self.get_argument('page', 0))
+        year = self.get_argument('year', '')
+        director = self.get_argument('director', '')
+        cast = self.get_argument('cast', '')
+        genre = self.get_argument('genre', '')
+
+        query = {}
+
+        if year is not '':
+            query['year'] = year
+        if director is not '':
+            query['directors'] = director
+        if cast is not '':
+            query['casts'] = cast
+        if genre is not '':
+            query['genres'] = genre
+
+        posts = self.db.movie.find(query).skip(10*page).limit(10)
+        self.render('index/index.html', posts=posts, page=page)
+
+
 class SearchHandler(BaseHandler):
     def get(self):
         self.render('index/search.html')

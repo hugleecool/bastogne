761875f9207ddcf33e38d056203fff8ccbe7f1c5
MaGuowei <imaguowei@gmail.com>
2014-03-07 19:14:55 +0800

    所有数据存储到本地

--- handler/post.py
+++ handler/post.py
@@ -1,35 +1,7 @@
-from tornado import gen
 from .base import BaseHandler
-import json
-from urllib.parse import urlencode
-import tornado.web
-from tornado import httpclient
-from tornado import gen
 
 
 class PostHandler(BaseHandler):
-    @gen.coroutine
     def get(self, pid):
         post = self.db.movie.find_one({'id': int(pid)})
-
-        url = 'https://api.douban.com/v2/movie/'
-        client = httpclient.AsyncHTTPClient()
-
-        search_data = {
-            'q': post['title'],
-            'start': 0,
-            'count': 1,
-        }
-        search_url = url + 'search?' + urlencode(search_data)
-        search_response = yield gen.Task(client.fetch, search_url)
-        search_result = json.loads(search_response.body.decode())
-
-        movie_id = search_result['subjects'][0]['id']
-
-        subject_url = url + 'subject/' + movie_id
-        response = yield gen.Task(client.fetch, subject_url)
-        result = json.loads(response.body.decode())
-
-        douban_movie = result
-        self.render('post/index.html', post=post, douban_movie=douban_movie)
-        self.finish()
+        self.render('post/index.html', post=post)
--- util/test.py
+++ util/test.py
@@ -7,13 +7,37 @@ collection = db.movie
 
 douban_movie = Movie()
 
-movies = collection.find().skip(12680).limit(5000)
-# movies = collection.find({'id': 236})
+movies = collection.find({'id': {'$gt': 16640}})
+# movies = collection.find()
 
 for m in movies:
     print(m['id'], m['title'], end=' ')
     try:
-        print(douban_movie.get_movie(m['title'])['id'])
+        mv = douban_movie.get_movie(m['title'])
+        print(mv['id'], end=' ')
+
+        directors = list()
+        for director in mv['directors']:
+            directors.append(director['name'])
+
+        casts = list()
+        for cast in mv['casts']:
+            casts.append(cast['name'])
+
+        data = {
+            'douban_id': mv['id'],
+            'alt': mv['alt'],
+            'rating': mv['rating']['average'],
+            'image': mv['images']['large'],
+            'year': mv['year'],
+            'genres': mv['genres'],
+            'countries': mv['countries'],
+            'summary': mv['summary'],
+            'directors': directors,
+            'casts': casts,
+        }
+        print(data)
+        collection.update({'id': m['id']}, {'$set': data})
     except IndexError:
         collection.remove({'id': m['id']})
         print('has remove')

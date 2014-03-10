2c436fc88efd5efdc86b60ef2f380eb8f87da376
MaGuowei <imaguowei@gmail.com>
2014-03-10 10:58:10 +0800

    更新数据

--- tools/movie.py
+++ tools/movie.py
@@ -3,6 +3,7 @@
 
 import json
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from pymongo import MongoClient
 
 
@@ -21,7 +22,38 @@ class Movie():
         movies = self.db.movie.find()
 
         for movie in movies:
-            print(movie['id'], movie['db_id'])
+            if movie.get('alt'):
+                pass
+            else:
+                try:
+                    douban_movie = self.get_subject_by_id(movie['db_id'])
+                    mv = dict()
+                    mv['alt'] = douban_movie['alt']
+                    mv['rating'] = douban_movie['rating']['average']
+                    mv['image'] = douban_movie['images']['large']
+                    mv['year'] = douban_movie['year']
+                    mv['countries'] = douban_movie['countries']
+                    mv['genres'] = douban_movie['genres']
+
+                    mv['directors'] = []
+
+                    for director in douban_movie['directors']:
+                        mv['directors'].append(director['name'])
+
+                    mv['casts'] = []
+
+                    for cast in douban_movie['casts']:
+                        mv['casts'].append(cast['name'])
+
+                    mv['summary'] = douban_movie['summary']
+
+                    self.db.movie.update({'id': movie['id']}, {'$set': mv})
+
+                    print(movie['id'], end=' ')
+                    print(mv)
+                except HTTPError as e:
+                    self.db.movie.remove({'id': movie['id']})
+                    print(movie['id'], e, 'has remove')
 
 
 if __name__ == '__main__':
